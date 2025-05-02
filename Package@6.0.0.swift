93ef70fc56b4e7ef88783b753a25b38f9a5b3c7a
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-02 00:53:35 -0400

    Add Package.swift specific for Swift 6

---
+++ Package@6.0.0.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ATCommonTools",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v13),
+        .tvOS(.v14),
+        .visionOS(.v1),
+        .watchOS(.v9)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ATCommonTools",
+            targets: ["ATCommonTools"]),
+        .library(
+            name: "ATCommonWeb",
+            targets: ["ATCommonWeb"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ATProtoKit/MultiformatsKit.git", .upToNextMajor(from: "0.0.1"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ATCommonTools",
+            dependencies: [
+                "ATCommonWeb",
+                .product(name: "MultiformatsKit", package: "multiformatskit")
+            ]
+        ),
+        .target(
+            name: "ATCommonWeb",
+            dependencies: [
+                .product(name: "MultiformatsKit", package: "multiformatskit")
+            ]
+        ),
+        .testTarget(
+            name: "ATCommonToolsTests",
+            dependencies: ["ATCommonTools", "ATCommonWeb"]
+        ),
+    ]
+)
+
