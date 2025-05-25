e80a5d9ac218e2e60e129161272584d20e5cffc8
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-24 20:41:32 -0400

    Convert ipld.ts, update conversion document

--- Conversion Documentation/common-web ipld conversion.md
+++ Conversion Documentation/common-web ipld conversion.md
@@ -14,12 +14,12 @@ We are using an `enum` for this.
 
 ## `export const jsonToIpld = (val: JsonValue): IpldValue`
 
-This type has been converted.
+This type has been converted. It is also used in the custom `Decodable` method.
 
 ## `export const ipldToJson = (val: IpldValue): JsonValue`
 
-This type has been converted.
+This type has been converted. It is also used in the custom `Encodable` method.
 
 ## `export const ipldEquals = (a: IpldValue, b: IpldValue): boolean`
 
-This is redundant and will not be converted. The `struct` that contains these objects, including the objects themselves, will conform to `Hashable` instead.
+This is redundant and will not be converted. The `enum` that contains these objects, including the objects themselves, will conform to `Equatable` instead.
--- Sources/ATCommonWeb/TypeScript-bound/IPLD.swift
+++ Sources/ATCommonWeb/TypeScript-bound/IPLD.swift
@@ -8,18 +8,19 @@
 import Foundation
 import MultiformatsKit
 
+/// A namespace contains types and utilities for working with IPLD (InterPlanetary Linked Data) values,
+/// which are used to represent and encode arbitrary, self-describing, content-addressable data.
+public enum IPLD: Equatable {
 
-public struct IPLD {
-
-    /// A type-safe and thread-safe representation of JSON-compatible values, used for encoding and
-    /// decoding arbitrary JSON data.
+    /// A type-safe and thread-safe representation of JSON-compatible values, used for encoding and decoding
+    /// arbitrary JSON data.
     public enum JSONValue: Codable, Equatable, Hashable {
 
         /// A `String` object.
         case string(String)
 
         /// A `Double` object.
-        case number(Double)
+        case number(Int)
 
         /// A `Bool` value.
         case bool(Bool)
@@ -40,7 +41,7 @@ public struct IPLD {
                 self = .nil
             } else if let bool = try? container.decode(Bool.self) {
                 self = .bool(bool)
-            } else if let number = try? container.decode(Double.self) {
+            } else if let number = try? container.decode(Int.self) {
                 self = .number(number)
             } else if let string = try? container.decode(String.self) {
                 self = .string(string)
@@ -65,147 +66,111 @@ public struct IPLD {
                     try container.encode(value)
                 case .nil:
                     try container.encodeNil()
-                case .array(let values):
-                    try container.encode(values)
-                case .dictionary(let dictionary):
-                    try container.encode(dictionary)
+                case .array(let value):
+                    try container.encode(value)
+                case .dictionary(let value):
+                    try container.encode(value)
             }
         }
     }
 
+    /// An enum representing all valid IPLD (InterPlanetary Linked Data) values.
+    public enum IPLDValue: Codable, Equatable, Hashable {
 
-    // TODO: Fix this to remove the errors.
-//    public enum IPLDValue: Codable, Equatable, Hashable {
-//
-//        /// A `String` object.
-//        case string(String)
-//
-//        /// A `Double` object.
-//        case number(Double)
-//
-//        /// A `Bool` object.
-//        case bool(Bool)
-//
-//        /// A `nil` object.
-//        case `nil`
-//
-//        /// An `Array` of `IPLDValue` objects.
-//        case array([IPLDValue])
-//
-//        /// A `Dictionary` object, with `IPLDValue` as the value.
-//        case dictionary([String: IPLDValue])
-//
-//        /// A `CID` object.
-//        //    case cid(CID)
-//
-//        /// A `Data` object.
-//        case bytes(Data)
-//
-//        public init(from decoder: Decoder) throws {
-//            let container = try decoder.singleValueContainer()
-//
-//            if container.decodeNil() {
-//                self = .nil
-//            } else if let bool = try? container.decode(Bool.self) {
-//                self = .bool(bool)
-//            } else if let number = try? container.decode(Double.self) {
-//                self = .number(number)
-//            } else if let string = try? container.decode(String.self) {
-//                self = .string(string)
-//            } else if let array = try? container.decode([IPLDValue].self) {
-//                self = .array(array)
-//            } else if let object = try? container.decode([String: IPLDValue].self) {
-//                self = .dictionary(object)
-//                //        } else if let cid = try? container.decode(CID.self) {
-//                //            self = .cid(cid)
-//            } else if let data = try? container.decode(Data.self) {
-//                self = .bytes(data)
-//            } else {
-//                throw DecodingError.dataCorruptedError(
-//                    in: container,
-//                    debugDescription: "Unsupported IPLD value"
-//                )
-//            }
-//        }
-//
-//        public func encode(to encoder: Encoder) throws {
-//            var container = encoder.singleValueContainer()
-//
-//            switch self {
-//                case .string(let value):
-//                    try container.encode(value)
-//                case .number(let value):
-//                    try container.encode(value)
-//                case .bool(let value):
-//                    try container.encode(value)
-//                case .nil:
-//                    try container.encodeNil()
-//                case .array(let values):
-//                    try container.encode(values)
-//                case .dictionary(let dictionary):
-//                    try container.encode(dictionary)
-//                    //            case .cid(let cid):
-//                    //                try container.encode(cid)
-//                case .bytes(let data):
-//                    try container.encode(data)
-//            }
-//        }
-//    }
-//
-//    public static func jsonToIpld(_ value: JSONValue) -> IPLDValue {
-//        switch value {
-//            case .string(let str):
-//                // Handle special IPLD markers
-//                if let json = try? JSONSerialization.jsonObject(with: Data(str.utf8)) as? [String: Any],
-//                   let onlyKey = json.keys.first, json.count == 1 {
-//                    if onlyKey == "$link", let linkStr = json["$link"] as? String, let cid = try? CID(string: linkStr) {
-//                        return .cid(cid)
-//                    }
-//                    if onlyKey == "$bytes", let base64Str = json["$bytes"] as? String, let data = Data(base64Encoded: base64Str) {
-//                        return .bytes(data)
-//                    }
-//                }
-//                return .string(str)
-//            case .number(let num):
-//                return .number(num)
-//            case .bool(let bool):
-//                return .bool(bool)
-//            case .nil:
-//                return .nil
-//            case .array(let arr):
-//                return .array(arr.map { jsonToIpld($0) })
-//            case .dictionary(let dict):
-//                var newDict: [String: IPLDValue] = [:]
-//                for (key, val) in dict {
-//                    newDict[key] = jsonToIpld(val)
-//                }
-//                return .dictionary(newDict)
-//        }
-//    }
-//
-//    public static func ipldToJson(_ value: IPLDValue) -> JSONValue {
-//        switch value {
-//            case .string(let str):
-//                return .string(str)
-//            case .number(let num):
-//                return .number(num)
-//            case .bool(let bool):
-//                return .bool(bool)
-//            case .nil:
-//                return .nil
-//            case .array(let arr):
-//                return .array(arr.map { ipldToJson($0) })
-//            case .dictionary(let dict):
-//                var newDict: [String: JSONValue] = [:]
-//                for (key, val) in dict {
-//                    newDict[key] = ipldToJson(val)
-//                }
-//                return .dictionary(newDict)
-//            case .cid(let cid):
-//                return .dictionary(["$link": .string(cid.description)])
-//            case .bytes(let data):
-//                return .dictionary(["$bytes": .string(data.base64EncodedString())])
-//        }
-//    }
+        /// A `JSONValue` object.
+        case jsonValue(JSONValue)
 
+        /// A `CID` object.
+        case cid(CID)
+
+        /// A `Data` object.
+        case bytes(Data)
+
+        /// An array of `IPLDValue` objects.
+        case array([IPLDValue])
+
+        /// A `Dictionary` object, with `IPLDValue` as the value.
+        case dictionary([String: IPLDValue])
+
+        public init(from decoder: Decoder) throws {
+            // Decode as JSONValue, then use the adapter to make IPLDValue.
+            let jsonValue = try JSONValue(from: decoder)
+            self = IPLD.jsonToIPLD(jsonValue)
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            // Convert self to JSONValue, then encode.
+            let jsonValue = IPLD.ipldToJSON(self)
+            try jsonValue.encode(to: encoder)
+        }
+    }
+
+    /// Converts a `JSONValue` to `IPLDValue`.
+    ///
+    /// - Parameter value: A `JSONValue` object.
+    /// - Returns: An `IPLDValue` object.
+    public static func jsonToIPLD(_ value: JSONValue) -> IPLDValue {
+        switch value {
+            case .array(let array):
+                return .array(array.map { jsonToIPLD($0) })
+            case .dictionary(let dictionary):
+                // Special case for { "$link": "..." }
+                if dictionary.count == 1, let jsonValue = dictionary["$link"], case let .string(linkString) = jsonValue {
+                    if let cid = try? CID(string: linkString) {
+                        return .cid(cid)
+                    }
+                }
+
+                // Special case for { "$bytes": "..." }
+                if dictionary.count == 1, let jsonValue = dictionary["$bytes"], case let .string(bytesString) = jsonValue {
+                    if let data = Data(base64Encoded: bytesString) {
+                        return .bytes(data)
+                    }
+                }
+
+                // Plain object: recursively convert each value.
+                var newDictionary: [String: IPLDValue] = [:]
+                for (key, value) in dictionary {
+                    newDictionary[key] = jsonToIPLD(value)
+                }
+
+                return .dictionary(newDictionary)
+            case .string(let value):
+                return .jsonValue(.string(value))
+            case .number(let value):
+                return .jsonValue(.number(value))
+            case .bool(let value):
+                return .jsonValue(.bool(value))
+            case .nil:
+                return .jsonValue(.nil)
+        }
+    }
+
+    /// Converts an `IPLDValue` to `JSONValue`.
+    ///
+    /// - Parameter value: A `IPLDValue` object.
+    /// - Returns: A `JSONValue` object.
+    public static func ipldToJSON(_ value: IPLDValue) -> JSONValue {
+        switch value {
+            case .array(let array):
+                return .array(array.map { ipldToJSON($0) })
+            case .dictionary(let dictionary):
+                var newDictionary: [String: JSONValue] = [:]
+                for (key, value) in dictionary {
+                    newDictionary[key] = ipldToJSON(value)
+                }
+
+                return .dictionary(newDictionary)
+            case .cid(let cid):
+                return .dictionary([
+                    "$link": .string((try? cid.encode()) ?? "")
+                ])
+            case .bytes(let data):
+                return .dictionary([
+                    "$bytes": .string(data.base64EncodedString())
+                ])
+            case .jsonValue(let json):
+                return json
+        }
+    }
 }
