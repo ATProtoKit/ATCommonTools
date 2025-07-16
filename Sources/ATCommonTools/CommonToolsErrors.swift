e59bed5156d23bc1076912740f1ff3844b57817a
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-07-16 02:37:11 -0400

    Add errors for ATCommonTools

---
+++ Sources/ATCommonTools/CommonToolsErrors.swift
@@ -0,0 +1,14 @@
+//
+//  CommonToolsErrors.swift
+//  ATCommonTools
+//
+//  Created by Christopher Jr Riley on 2025-07-16.
+//
+
+import Foundation
+
+public enum DecodingError: Error {
+
+    ///
+    case unsupportedEncoding(String)
+}
